9ef0f7afc036a9210b6c804aed57002616fdea48
AleksMMM <gol8an@yandex.ru>
2021-10-30 18:08:09 +0300

    домашняя работа номер один. Создал переменные примитивного типа

---
+++ src/main/java/Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+
+    public static void main(String[] args) {
+
+        byte bytes = 100;
+        short shortes = 234;
+        int intes = 1000;
+        long lones = 10000L;
+        float f = 10f;
+        double doub = 10.00;
+        char ch = 'r';
+        boolean bol = true;
+
+        System.out.println("вывел все числа");
+    }
+}
