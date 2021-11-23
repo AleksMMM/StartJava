a2a078297ddc508832f73490dc02223d8224a667
AMyshllyaev <AValMyshlyaev@sberbank.ru>
2021-11-23 22:39:18 +0300

    пытаюсь реализовать через масив попытки, н оу меня почему то не сохраняются попытки и при выводе не обнуляется массив

--- src/com/startjava/lesson_2_3/game/GuessNumber.java
+++ src/com/startjava/lesson_2_3/game/GuessNumber.java
@@ -19,49 +19,62 @@ public class GuessNumber {
         System.out.println("Добро пожаловать в игру \"Угадай число\".");
         System.out.println("Играют два игрока.");
         System.out.println("Нужно вводить число в интревале от 1 до 100, кто первый угадал тот и выиграл. ");
+        System.out.println("у каждого игрока по 10 попыток");
 
         do {
             System.out.println(firstPlayer.getName() + " введите число : ");
             firstPlayer.setNumber(console.nextInt());
+            firstPlayer.setNumberArray(firstPlayer.getNumber());
 
             if (firstPlayer.getNumber() < numberRandom) {
                 System.out.println("Число первого игрока меньше искомого");
                 System.out.println(secondPlayer.getName() + " введите число : ");
                 secondPlayer.setNumber(console.nextInt());
+                secondPlayer.setNumberArray(secondPlayer.getNumber());
+                secondPlayer.setChansPlayer(secondPlayer.getChansPlayer() + 1);
 
             } else if (firstPlayer.getNumber() > numberRandom) {
                 System.out.println("Число первого игрока больше искомого");
                 System.out.println();
                 System.out.println(secondPlayer.getName() + " введите число : ");
                 secondPlayer.setNumber(console.nextInt());
-            } else if(firstPlayer.getNumber() == numberRandom) {
-                System.out.println("Поздравляем!!!Победил первый игрок!!! Это было число  - " + numberRandom);
+                secondPlayer.setNumberArray(secondPlayer.getNumber());
+            } else if (firstPlayer.getNumber() == numberRandom) {
+                System.out.println("Игрок " + firstPlayer.getName() + " угадал число " + numberRandom + " с " + firstPlayer.getChansPlayer() + " попытки");
+                System.out.println();
+                secondPlayer.arraysNew(secondPlayer);
+                firstPlayer.arraysNew(firstPlayer);
                 break;
+            } else if (firstPlayer.getChansPlayer() == 9) {
+                System.out.println("У " + firstPlayer.getName() + " закончились попытки");
+                System.out.println("не угадал :(  Попробуйте еще.\n");
+                secondPlayer.arraysNew(secondPlayer);
+                firstPlayer.arraysNew(firstPlayer);
             }
 
-            System.out.println(secondPlayer.getName() + " введите число : ");
-            secondPlayer.setNumber(console.nextInt());
             if (secondPlayer.getNumber() < numberRandom) {
                 System.out.println("Число второго игрока меньше искомого\n");
-                System.out.println();
-                System.out.println(firstPlayer.getName() + " введите число : ");
-                firstPlayer.setNumber(console.nextInt());
 
-            }
-            if (secondPlayer.getNumber() > numberRandom){
+
+            } else if (secondPlayer.getNumber() > numberRandom) {
                 System.out.println("Число второго игрока больше искомого\n");
-                System.out.println();
-                System.out.println(secondPlayer.getName() + " введите число : ");
-                firstPlayer.setNumber(console.nextInt());
+
+
             }
 
-            if(secondPlayer.getNumber() == numberRandom) {
-                System.out.println("Поздравляем!!!Победил второй игрок!!! Это было число  - " + numberRandom);
+            if (secondPlayer.getNumber() == numberRandom) {
+                System.out.println("Игрок " + secondPlayer.getName() + " угадал число " + numberRandom + " с " + secondPlayer.getChansPlayer() + " попытки");
                 break;
-            }  else {
+            } else if (secondPlayer.getChansPlayer() == 9) {
+                System.out.println("У " + secondPlayer.getName() + " закончились попытки");
                 System.out.println("не угадал :(  Попробуйте еще.\n");
+                secondPlayer.arraysNew(secondPlayer);
+                firstPlayer.arraysNew(firstPlayer);
             }
 
-            } while (true);
+        } while (true);
     }
+
+
 }
+
--- src/com/startjava/lesson_2_3/game/Player.java
+++ src/com/startjava/lesson_2_3/game/Player.java
@@ -1,11 +1,31 @@
 package com.startjava.lesson_2_3.game;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Player {
     private String name;
     private int number;
+    private int[] numberArray;
+    private int chansPlayer = 0;
+    Scanner console;
 
     public Player(String name) {
        this.name = name;
+       this.numberArray = new int[10];
+    }
+
+    public void arraysNew(Player player) {
+        if (player.chansPlayer < 9) {
+            for (Integer in: player.numberArray) {
+                System.out.print(in + " ");
+            }
+        } else {
+            for (Integer in: player.numberArray) {
+                System.out.print(in + " ");
+            }
+        }
+        System.out.println();
     }
 
     public String getName() {
@@ -18,5 +38,21 @@ public class Player {
 
     public void setNumber(int number) {
         this.number = number;
+    }
+
+    public int[] getNumberArray() {
+        return numberArray;
+    }
+
+    public void setNumberArray(int number) {
+        numberArray[chansPlayer] = number;
+    }
+
+    public int getChansPlayer() {
+        return chansPlayer;
+    }
+
+    public void setChansPlayer(int chansPlayer) {
+        this.chansPlayer = chansPlayer;
     }
 }
